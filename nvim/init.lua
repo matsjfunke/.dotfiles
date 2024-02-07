3b4f8457acd9d0e39b0ace39085bf3460001bb48
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-02-07 17:59:28 +0100

    inital nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
--- LazyVim Packagemanager
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,4 +13,24 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup({
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
+
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+	{ 'joshdick/onedark.vim', priority = 1000 }
+}, 
+{})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "rust", "css", "dockerfile", "go", "json", "markdown", "ruby", "sql", "yaml" },
+	highlight = { enable = true },
+        indent = { enable = true },
+	})
+
+vim.cmd("colorscheme onedark")
