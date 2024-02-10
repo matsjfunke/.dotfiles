cd634aa135afacd2d08da674936a61345e2bb565
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-02-10 01:37:20 +0100

    treesitter finetune

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "rust", "css", "dockerfile", "go", "json", "markdown", "ruby", "sql", "yaml" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
