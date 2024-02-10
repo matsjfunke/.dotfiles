e44dfedb7f348a0d90a7f94e226571dbb742dfb7
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-02-10 01:22:50 +0100

    clean init.lua, theme works, vim setting tweaks

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -9,3 +9,17 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {noremap = true})
 vim.api.nvim_set_keymap('v', 'kj', '<Esc>', {noremap = true})
 vim.api.nvim_set_keymap('c', 'kj', '<Esc>', {noremap = true})
+
+-- Tab
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- sync to system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- scroll cursor middle
+vim.opt.scrolloff = 999
+
+-- colors
+vim.opt.termguicolors = true
