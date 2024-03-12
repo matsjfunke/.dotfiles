a8ecc669faf30747b5d065a1bb78a211ca1f3035
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-03-12 17:09:15 +0100

    autocomplete like supertab

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
     "hrsh7th/nvim-cmp",
 
@@ -9,7 +15,6 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip"
     },
-
     config = function()
         local cmp = require("cmp")
 
@@ -23,21 +28,38 @@ return {
             end,
             },
             window = {
-                -- completion = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
                 -- documentation = cmp.config.window.bordered(),
             },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            }),
+            mapping = {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                  elseif has_words_before() then
+                    cmp.complete()
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                  if cmp.visible() then
+                    cmp.select_prev_item()
+                  elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" }),
+            },
+
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "nvim_lua" },
                 { name = "luasnip" },
-            }, 
+            },
             {
                 { name = "buffer" },
                 { name = "path" },
@@ -52,6 +74,5 @@ return {
                 { name = "cmdline" },
             }),
         })
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
     end
 }
