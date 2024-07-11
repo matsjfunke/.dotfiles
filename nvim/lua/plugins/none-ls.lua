ae6370e84d0ebc31b53c936c86ad5071ffd98d54
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-07-12 00:06:37 +0200

    fixed formatter line length

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,7 +1,7 @@
 return {
-    -- wraps linters/formatters to make them seem like an normal lsp to nvim-lspconfig
-	-- still need to install linters and formatters with mason 
-        -- :Mason   =>    /linter_name <CR>     =>      i (to install linter/fromatter)
+	-- wraps linters/formatters to make them seem like an normal lsp to nvim-lspconfig
+	-- still need to install linters and formatters with mason
+	-- :Mason   =>    /linter_name <CR>     =>      i (to install linter/fromatter)
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
@@ -10,24 +10,24 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length","180"}}),
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.formatting.rust_analyzer,
+				null_ls.builtins.formatting.black.with({ extra_args = { "--line-length", "180" } }),
+				null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "180" } }),
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.rust_analyzer,
 			},
 		})
 
-        -- Autoformat on save
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		-- Autoformat on save
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            pattern = "*",
-            callback = function()
-                vim.lsp.buf.format({ async = true })
-            end,
-        })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			pattern = "*",
+			callback = function()
+				vim.lsp.buf.format({ async = true })
+			end,
+		})
 
 		vim.keymap.set("n", "<leader>sf", vim.lsp.buf.format, {})
 	end,
