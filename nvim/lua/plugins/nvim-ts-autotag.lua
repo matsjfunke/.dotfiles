8b63e5919c1858cad94d32a4244205d7484e6d64
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-11-05 11:10:52 +0100

    removed leagacy autotag setup with opts

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,15 +1,25 @@
 return {
-	-- nvim-ts-autotag for auto closing and renaming HTML tags
-	{
-		"windwp/nvim-ts-autotag",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		opts = {
-			enable_close = true, -- Auto close tags
-			enable_rename = true, -- Auto rename pairs of tags
-			enable_close_on_slash = false, -- Auto close on trailing </
-		},
-		config = function(_, opts)
-			require("nvim-ts-autotag").setup(opts)
-		end,
-	},
+  -- nvim-ts-autotag for auto closing and renaming HTML tags
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup({
+        filetypes = {
+          "html",
+          "xml",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "tsx",
+          "jsx",
+          "rescript",
+        },
+        -- Add any additional options if needed
+      })
+    end,
+  },
 }
