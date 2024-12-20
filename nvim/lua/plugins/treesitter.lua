4b0192d7f94db538bb8531293f3a143558fb3a9d
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-12-21 00:08:42 +0100

    fixed highlighting for mdx files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,42 @@
 return {
-  --nvim-treesitter syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {
-          "c",
-          "cpp",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "javascript",
-          "typescript",
-          "tsx",
-          "html",
-          "python",
-          "rust",
-          "css",
-          "dockerfile",
-          "go",
-          "json",
-          "markdown",
-          "ruby",
-          "sql",
-          "yaml",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	--nvim-treesitter syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			-- Set .mdx files to use markdown filetype
+			vim.cmd([[
+        autocmd BufRead,BufNewFile *.mdx set filetype=markdown
+      ]])
+
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"c",
+					"cpp",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"typescript",
+					"tsx",
+					"html",
+					"python",
+					"rust",
+					"css",
+					"dockerfile",
+					"go",
+					"json",
+					"markdown",
+					"ruby",
+					"sql",
+					"yaml",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
